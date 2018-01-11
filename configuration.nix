1f8f3fcd4a9fb5724923ee0efe1fd8cb23364185
Francesco Gazzetta <francygazz@gmail.com>
2018-01-12 00:26:27 +0100

    Serve haskell-ita website

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,21 @@
 { config, pkgs, ... }:
 {
+  imports = [ ./mighttpd2.nix ];
   nixpkgs.overlays = [ (import ./overlay) ];
+  services.mighttpd2.enable = true;
+  services.mighttpd2.config = ''
+        # Example configuration for Mighttpd 2
+        Port: 80
+        # IP address or "*"
+        Host: *
+        Index_File: index.html
+        Service: 0 # 0 is HTTP only, 1 is HTTPS only, 2 is both
+      '';
+  services.mighttpd2.routing = ''
+        [haskell-ita.it]
+        / -> ${pkgs.haskell-ita-website}
+        [*]
+        / -> ${pkgs.haskell-ita-website}
+      '';
+  networking.firewall.allowedTCPPorts = [ 80 ];
 }
