9cbeb5f5530af048cc5870c9bd1cec600af2c197
Francesco Gazzetta <fgaz@fgaz.me>
2020-11-01 17:22:35 +0100

    Update webpager

--- overlay/pkgs/webpager/default.nix
+++ overlay/pkgs/webpager/default.nix
@@ -7,8 +7,8 @@ mkDerivation {
   src = fetchFromGitHub {
     owner = "fgaz";
     repo = "webpager";
-    rev = "f314f239328aff94af96bc6ab49964f66a44dae8";
-    sha256 = "104wr5ja77i9g64ay53kiblq74spksq2h4wry884ny2xs1yn7w2r";
+    rev = "0d16df225a9c8f38d37f3ccd5bfca893ed6b8169";
+    sha256 = "1hp8vja3b28llbwwixms3hm3rii101i67n0vry8apwmq9k5yxylp";
   };
   libraryHaskellDepends = [
     base bytestring data-default-class hashable http-types lucid text
